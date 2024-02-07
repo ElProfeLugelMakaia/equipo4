2d7d93f6a3580c827f202a25ce25ae7368f1e152
d3lt409 <manuelfer1996@gmail.com>
2024-02-07 13:13:24 -0500

    update: SecurityConfiguration

--- src/main/java/com/makaia/grupo4/entrevista/security/SecurityConfiguration.java
+++ src/main/java/com/makaia/grupo4/entrevista/security/SecurityConfiguration.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @EnableWebSecurity
 @Configuration
@@ -24,13 +27,28 @@ public class SecurityConfiguration {
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        httpSecurity = httpSecurity
+                .csrf(AbstractHttpConfigurer::disable).cors(AbstractHttpConfigurer::disable);
+
+        httpSecurity = httpSecurity
+                .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // Set unauthorized requests exception handler
+        httpSecurity = httpSecurity
+                .exceptionHandling(handling -> handling
+                        .authenticationEntryPoint(
+                                (request, response, ex) -> {
+                                    response.sendError(
+                                            HttpServletResponse.SC_UNAUTHORIZED,
+                                            ex.getMessage());
+                                }));
+
+        httpSecurity = httpSecurity
                 .authorizeHttpRequests((authorize) -> {
                     authorize
-                            .requestMatchers("/public/**", "/api/v1/users/login", "/api/v1/users/forgotPassword",
-                                    "/api/v1/users/signup")
+                            .requestMatchers("/public/**", "api/v1/users/login", "api/v1/users/forgotPassword",
+                                    "api/v1/users/signup")
                             .permitAll()
-                            .requestMatchers("/api/v1/**").permitAll()
                             .anyRequest().authenticated();
                 });
 
